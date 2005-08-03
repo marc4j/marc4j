a963b8c9efba5ed096d8e39d929af40cd2700493
bpeters <bpeters>
2005-08-03 19:07:27 +0000

    Removed SAXException handling

--- src/org/marc4j/util/MarcXmlDriver.java
+++ src/org/marc4j/util/MarcXmlDriver.java
@@ -1,4 +1,4 @@
-//$Id: MarcXmlDriver.java,v 1.1 2005/05/04 10:06:47 bpeters Exp $
+//$Id: MarcXmlDriver.java,v 1.2 2005/08/03 19:07:27 bpeters Exp $
 /**
  * Copyright (C) 2005 Bas Peters
  *
@@ -43,182 +43,196 @@ import org.marc4j.converter.impl.AnselToUnicode;
 import org.marc4j.converter.impl.Iso5426ToUnicode;
 import org.marc4j.converter.impl.Iso6937ToUnicode;
 import org.marc4j.marc.Record;
-import org.xml.sax.SAXException;
 
 /**
  * Provides a basic driver to convert MARC records to MARCXML.
  * <p>
  * The following example reads input.mrc and writes output to the console:
- * <pre>   java org.marc4j.util.MarcXmlDriver input.mrc</pre>
+ * 
+ * <pre>
+ *    java org.marc4j.util.MarcXmlDriver input.mrc
+ * </pre>
+ * 
  * <p>
  * The following example reads input.mrc, converts MARC-8 and writes output in
  * UTF-8 to output.xml:
- * <pre>   java org.marc4j.util.MarcXmlDriver -convert MARC8 -out output.xml input.mrc</pre>
+ * 
+ * <pre>
+ *    java org.marc4j.util.MarcXmlDriver -convert MARC8 -out output.xml input.mrc
+ * </pre>
+ * 
  * <p>
  * It is possible to post-process the result using an XSLT stylesheet. The
  * following example converts MARC to MODS:
- * <pre>   java org.marc4j.util.MarcXmlDriver -convert MARC8 -xsl http://www.loc.gov/standards/mods/v3/MARC21slim2MODS3.xsl -out modsoutput.xml input.mrc</pre>
+ * 
+ * <pre>
+ *    java org.marc4j.util.MarcXmlDriver -convert MARC8 -xsl http://www.loc.gov/standards/mods/v3/MARC21slim2MODS3.xsl -out modsoutput.xml input.mrc
+ * </pre>
+ * 
  * <p>
  * For usage, run from the command-line with the following command:
- * <pre>   java org.marc4j.util.MarcXmlDriver -usage</pre>
+ * 
+ * <pre>
+ *    java org.marc4j.util.MarcXmlDriver -usage
+ * </pre>
+ * 
  * <p>
  * Check the home page for <a href="http://www.loc.gov/standards/marcxml/">
  * MARCXML </a> for more information about the MARCXML format.
  * 
  * @author Bas Peters
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  *  
  */
 public class MarcXmlDriver {
 
-  /**
-   * Provides a static entry point.
-   * 
-   * <p>
-   * Arguments:
-   * </p>
-   * <ul>
-   * <li>-xsl &lt;stylesheet URL&gt; - post-process using XSLT-stylesheet</li>
-   * <li>-out &lt;output file&gt; - write to output file</li>
-   * <li>-convert &lt;encoding&gt; - convert &lt;encoding&gt; to UTF-8</li>
-   * <li>-normalize - perform Unicode normalization</li>
-   * <li>-usage - show usage</li>
-   * <li>&lt;input file&gt; - input file with MARC records
-   * </ul>
-   */
-  public static void main(String args[]) {
-    long start = System.currentTimeMillis();
+	/**
+	 * Provides a static entry point.
+	 * 
+	 * <p>
+	 * Arguments:
+	 * </p>
+	 * <ul>
+	 * <li>-xsl &lt;stylesheet URL&gt; - post-process using XSLT-stylesheet
+	 * </li>
+	 * <li>-out &lt;output file&gt; - write to output file</li>
+	 * <li>-convert &lt;encoding&gt; - convert &lt;encoding&gt; to UTF-8</li>
+	 * <li>-normalize - perform Unicode normalization</li>
+	 * <li>-usage - show usage</li>
+	 * <li>&lt;input file&gt; - input file with MARC records
+	 * </ul>
+	 */
+	public static void main(String args[]) {
+		long start = System.currentTimeMillis();
 
-    String input = null;
-    String output = null;
-    String stylesheet = null;
-    String convert = null;
-    boolean normalize = false;
+		String input = null;
+		String output = null;
+		String stylesheet = null;
+		String convert = null;
+		boolean normalize = false;
 
-    for (int i = 0; i < args.length; i++) {
-      if (args[i].equals("-xsl")) {
-        if (i == args.length - 1) {
-          usage();
-        }
-        stylesheet = args[++i].trim();
-      } else if (args[i].equals("-out")) {
-        if (i == args.length - 1) {
-          usage();
-        }
-        output = args[++i].trim();
-      } else if (args[i].equals("-convert")) {
-        if (i == args.length - 1) {
-          usage();
-        }
-        convert = args[++i].trim();
-      } else if (args[i].equals("-normalize")) {
-        normalize = true;
-      } else if (args[i].equals("-usage")) {
-        usage();
-      } else if (args[i].equals("-help")) {
-        usage();
-      } else {
-        input = args[i].trim();
+		for (int i = 0; i < args.length; i++) {
+			if (args[i].equals("-xsl")) {
+				if (i == args.length - 1) {
+					usage();
+				}
+				stylesheet = args[++i].trim();
+			} else if (args[i].equals("-out")) {
+				if (i == args.length - 1) {
+					usage();
+				}
+				output = args[++i].trim();
+			} else if (args[i].equals("-convert")) {
+				if (i == args.length - 1) {
+					usage();
+				}
+				convert = args[++i].trim();
+			} else if (args[i].equals("-normalize")) {
+				normalize = true;
+			} else if (args[i].equals("-usage")) {
+				usage();
+			} else if (args[i].equals("-help")) {
+				usage();
+			} else {
+				input = args[i].trim();
 
-        // Must be last arg
-        if (i != args.length - 1) {
-          usage();
-        }
-      }
-    }
-    if (input == null) {
-      usage();
-    }
+				// Must be last arg
+				if (i != args.length - 1) {
+					usage();
+				}
+			}
+		}
+		if (input == null) {
+			usage();
+		}
 
-    InputStream in = null;
-    try {
-      in = new FileInputStream(input);
-    } catch (FileNotFoundException e) {
-      e.printStackTrace();
-    }
-    MarcStreamReader reader = new MarcStreamReader(in);
+		InputStream in = null;
+		try {
+			in = new FileInputStream(input);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		MarcStreamReader reader = new MarcStreamReader(in);
 
-    OutputStream out = null;
-    if (output != null)
-      try {
-        out = new FileOutputStream(output);
-      } catch (FileNotFoundException e) {
-        e.printStackTrace();
-      }
-    else
-      out = System.out;
+		OutputStream out = null;
+		if (output != null)
+			try {
+				out = new FileOutputStream(output);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		else
+			out = System.out;
 
-    MarcXmlWriter writer = null;
+		MarcXmlWriter writer = null;
 
-    if (stylesheet == null) {
-      if (convert != null)
-        writer = new MarcXmlWriter(out, "UTF8");
-      else
-        try {
-          writer = new MarcXmlWriter(out);
-        } catch (SAXException e) {
-          e.printStackTrace();
-        }
-    } else {
-      Writer outputWriter = null;
-      if (convert != null) {
-        try {
-          outputWriter = new OutputStreamWriter(out, "UTF8");
-        } catch (UnsupportedEncodingException e) {
-          e.printStackTrace();
-        }
-        outputWriter = new BufferedWriter(outputWriter);
-      } else {
-        outputWriter = new OutputStreamWriter(out);
-        outputWriter = new BufferedWriter(outputWriter);
-      }
-      Result result = new StreamResult(outputWriter);
-      Source source = new StreamSource(stylesheet);
-      writer = new MarcXmlWriter(result, source);
-    }
-    writer.setIndent(true);
+		if (stylesheet == null) {
+			if (convert != null)
+				writer = new MarcXmlWriter(out, "UTF8");
+			else
+				writer = new MarcXmlWriter(out);
+		} else {
+			Writer outputWriter = null;
+			if (convert != null) {
+				try {
+					outputWriter = new OutputStreamWriter(out, "UTF8");
+				} catch (UnsupportedEncodingException e) {
+					e.printStackTrace();
+				}
+				outputWriter = new BufferedWriter(outputWriter);
+			} else {
+				outputWriter = new OutputStreamWriter(out);
+				outputWriter = new BufferedWriter(outputWriter);
+			}
+			Result result = new StreamResult(outputWriter);
+			Source source = new StreamSource(stylesheet);
+			writer = new MarcXmlWriter(result, source);
+		}
+		writer.setIndent(true);
 
-    if (convert != null) {
-      CharConverter charconv = null;
-      if (Constants.MARC_8_ENCODING.equals(convert))
-        charconv = new AnselToUnicode();
-      else if (Constants.ISO5426_ENCODING.equals(convert))
-        charconv = new Iso5426ToUnicode();
-      else if (Constants.ISO6937_ENCODING.equals(convert))
-        charconv = new Iso6937ToUnicode();
-      else {
-        System.err.println("Unknown character set");
-        System.exit(1);
-      }
-      writer.setConverter(charconv);
-    }
-    
-    if (normalize)
-      writer.setUnicodeNormalization(true);
+		if (convert != null) {
+			CharConverter charconv = null;
+			if (Constants.MARC_8_ENCODING.equals(convert))
+				charconv = new AnselToUnicode();
+			else if (Constants.ISO5426_ENCODING.equals(convert))
+				charconv = new Iso5426ToUnicode();
+			else if (Constants.ISO6937_ENCODING.equals(convert))
+				charconv = new Iso6937ToUnicode();
+			else {
+				System.err.println("Unknown character set");
+				System.exit(1);
+			}
+			writer.setConverter(charconv);
+		}
 
-    while (reader.hasNext()) {
-      Record record = reader.next();
-      writer.write(record);
-    }
-    writer.close();
+		if (normalize)
+			writer.setUnicodeNormalization(true);
 
-    System.err.println("Total time: " + (System.currentTimeMillis() - start)
-        + " miliseconds");
-  }
+		while (reader.hasNext()) {
+			Record record = reader.next();
+			writer.write(record);
+		}
+		writer.close();
 
-  private static void usage() {
-    System.err.println("MARC4J, Copyright (C) 2002-2005 Bas Peters");
-    System.err
-        .println("Usage: org.marc4j.util.MarcXmlDriver [-options] <file.mrc>");
-    System.err.println("       -convert <encoding> = Converts <encoding> to UTF-8");
-    System.err.println("       -normalize = perform Unicode normalization");    
-    System.err
-        .println("       -xsl <file> = Post-process MARCXML using XSLT stylesheet <file>");
-    System.err.println("       -out <file> = Output using <file>");
-    System.err.println("       -usage or -help = this message");
-    System.err.println("The program outputs well-formed MARCXML");
-    System.err.println("See http://marc4j.tigris.org for more information.");
-    System.exit(1);
-  }
+		System.err.println("Total time: "
+				+ (System.currentTimeMillis() - start) + " miliseconds");
+	}
+
+	private static void usage() {
+		System.err.println("MARC4J, Copyright (C) 2002-2005 Bas Peters");
+		System.err
+				.println("Usage: org.marc4j.util.MarcXmlDriver [-options] <file.mrc>");
+		System.err
+				.println("       -convert <encoding> = Converts <encoding> to UTF-8");
+		System.err.println("       -normalize = perform Unicode normalization");
+		System.err
+				.println("       -xsl <file> = Post-process MARCXML using XSLT stylesheet <file>");
+		System.err.println("       -out <file> = Output using <file>");
+		System.err.println("       -usage or -help = this message");
+		System.err.println("The program outputs well-formed MARCXML");
+		System.err
+				.println("See http://marc4j.tigris.org for more information.");
+		System.exit(1);
+	}
 
 }
