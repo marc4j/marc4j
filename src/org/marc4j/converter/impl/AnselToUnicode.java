634974c240230983be95303a7a1321d0ded7ab42
bpeters <bpeters>
2005-11-28 16:50:22 +0000

    Added contructors to enable initialization with a different code table file

--- src/org/marc4j/converter/impl/AnselToUnicode.java
+++ src/org/marc4j/converter/impl/AnselToUnicode.java
@@ -1,4 +1,4 @@
-// $Id: AnselToUnicode.java,v 1.1 2005/05/04 10:06:46 bpeters Exp $
+// $Id: AnselToUnicode.java,v 1.2 2005/11/28 16:50:22 bpeters Exp $
 /**
  * Copyright (C) 2002 Bas Peters (mail@bpeters.com)
  *
@@ -20,6 +20,7 @@
  */
 package org.marc4j.converter.impl;
 
+import java.io.InputStream;
 import java.util.Vector;
 
 import org.marc4j.converter.CharConverter;
@@ -30,240 +31,272 @@ import org.marc4j.converter.CharConverter;
  * </p>
  * 
  * <p>
- * The MARC-8 to Unicode mapping used is the version with the March 2005 revisions.
+ * The MARC-8 to Unicode mapping used is the version with the March 2005
+ * revisions.
  * </p>
+ * 
  * @author Bas Peters
  * @author Corey Keith
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class AnselToUnicode implements CharConverter {
 
-  class Queue extends Vector {
+    class Queue extends Vector {
+
+        /**
+         * Puts an item into the queue.
+         * 
+         * @param item
+         *            the item to be put into the queue.
+         */
+        public Object put(Object item) {
+            addElement(item);
+
+            return item;
+        }
+
+        /**
+         * Gets an item from the front of the queue.
+         */
+        public Object get() {
+            Object obj;
+            int len = size();
+
+            obj = peek();
+            removeElementAt(0);
+
+            return obj;
+        }
+
+        /**
+         * Peeks at the front of the queue.
+         */
+        public Object peek() {
+            int len = size();
+
+            return elementAt(0);
+        }
+
+        /**
+         * Returns true if the queue is empty.
+         */
+        public boolean empty() {
+            return size() == 0;
+        }
+    }
+
+    class CodeTracker {
+        int offset;
+
+        int g0;
+
+        int g1;
+
+        boolean multibyte;
+
+        public String toString() {
+            return "Offset: " + offset + " G0: " + Integer.toHexString(g0)
+                    + " G1: " + Integer.toHexString(g1) + " Multibyte: "
+                    + multibyte;
+        }
+    }
+
+    protected CodeTable ct;
+
+    protected boolean loadedMultibyte = false;
 
     /**
-     * Puts an item into the queue.
+     * Creates a new instance and loads the MARC4J supplied Ansel/Unicode
+     * conversion tables based on the official LC tables.
+     *  
+     */
+    public AnselToUnicode() {
+        this(AnselToUnicode.class
+                .getResourceAsStream("resources/codetablesnocjk.xml"));
+    }
+
+    /**
+     * Constructs an instance with the specified pathname.
      * 
-     * @param item
-     *          the item to be put into the queue.
+     * Use this constructor to create an instance with a customized code table
+     * mapping. The mapping file should follow the structure of LC's XML MARC-8
+     * to Unicode mapping (see:
+     * http://www.loc.gov/marc/specifications/codetables.xml).
+     *  
      */
-    public Object put(Object item) {
-      addElement(item);
-
-      return item;
+    public AnselToUnicode(String pathname) {
+        ct = new CodeTable(pathname);
+        loadedMultibyte = true;
     }
 
     /**
-     * Gets an item from the front of the queue.
+     * Constructs an instance with the specified input stream.
+     * 
+     * Use this constructor to create an instance with a customized code table
+     * mapping. The mapping file should follow the structure of LC's XML MARC-8
+     * to Unicode mapping (see:
+     * http://www.loc.gov/marc/specifications/codetables.xml).
+     *  
      */
-    public Object get() {
-      Object obj;
-      int len = size();
-
-      obj = peek();
-      removeElementAt(0);
-
-      return obj;
+    public AnselToUnicode(InputStream in) {
+        ct = new CodeTable(in);
+        loadedMultibyte = true;
     }
 
     /**
-     * Peeks at the front of the queue.
+     * Loads the entire maping (including multibyte characters) from the Library
+     * of Congress.
      */
-    public Object peek() {
-      int len = size();
-
-      return elementAt(0);
+    private void loadMultibyte() {
+        ct = new CodeTable(getClass().getResourceAsStream(
+                "resources/codetables.xml"));
     }
 
-    /**
-     * Returns true if the queue is empty.
-     */
-    public boolean empty() {
-      return size() == 0;
-    }
-  }
-
-  class CodeTracker {
-    int offset;
-
-    int g0;
-
-    int g1;
-
-    boolean multibyte;
-
-    public String toString() {
-      return "Offset: " + offset + " G0: " + Integer.toHexString(g0) + " G1: "
-          + Integer.toHexString(g1) + " Multibyte: " + multibyte;
-    }
-  }
-
-  protected CodeTable ct;
-
-  protected boolean loadedMultibyte = false;
-
-  /**
-   * Default constructor.
-   *
-   */
-  public AnselToUnicode() {
-    ct = new CodeTable(getClass().getResourceAsStream(
-        "resources/codetablesnocjk.xml"));
-  }
-
-  /**
-   * Loads the entire maping (including multibyte characters) from the Library
-   * of Congress.
-   */
-  public void loadMultibyte() {
-    ct = new CodeTable(getClass().getResourceAsStream(
-        "resources/codetables.xml"));
-  }
-
-  private void checkMode(char[] data, CodeTracker cdt) {
-    while (cdt.offset < data.length && isEscape(data[cdt.offset])) {
-      switch (data[cdt.offset + 1]) {
-      case 0x28:
-      case 0x2c:
-        cdt.g0 = data[cdt.offset + 2];
-        cdt.offset += 3;
-        cdt.multibyte = false;
-        break;
-      case 0x29:
-      case 0x2d:
-        cdt.g1 = data[cdt.offset + 2];
-        cdt.offset += 3;
-        cdt.multibyte = false;
-        break;
-      case 0x24:
-        cdt.multibyte = true;
-        if (!loadedMultibyte) {
-          loadMultibyte();
-          loadedMultibyte = true;
+    private void checkMode(char[] data, CodeTracker cdt) {
+        while (cdt.offset < data.length && isEscape(data[cdt.offset])) {
+            switch (data[cdt.offset + 1]) {
+            case 0x28:
+            case 0x2c:
+                cdt.g0 = data[cdt.offset + 2];
+                cdt.offset += 3;
+                cdt.multibyte = false;
+                break;
+            case 0x29:
+            case 0x2d:
+                cdt.g1 = data[cdt.offset + 2];
+                cdt.offset += 3;
+                cdt.multibyte = false;
+                break;
+            case 0x24:
+                cdt.multibyte = true;
+                if (!loadedMultibyte) {
+                    loadMultibyte();
+                    loadedMultibyte = true;
+                }
+                switch (data[cdt.offset + 1]) {
+                case 0x29:
+                case 0x2d:
+                    cdt.g1 = data[cdt.offset + 3];
+                    cdt.offset += 4;
+                    break;
+                case 0x2c:
+                    cdt.g0 = data[cdt.offset + 3];
+                    cdt.offset += 4;
+                    break;
+                default:
+                    cdt.g0 = data[cdt.offset + 2];
+                    cdt.offset += 3;
+                    break;
+                }
+                break;
+            case 0x67:
+            case 0x62:
+            case 0x70:
+                cdt.g0 = data[cdt.offset + 1];
+                cdt.offset += 2;
+                cdt.multibyte = false;
+                break;
+            case 0x73:
+                cdt.g0 = 0x42;
+                cdt.offset += 2;
+                cdt.multibyte = false;
+                break;
+            }
         }
-        switch (data[cdt.offset + 1]) {
-        case 0x29:
-        case 0x2d:
-          cdt.g1 = data[cdt.offset + 3];
-          cdt.offset += 4;
-          break;
-        case 0x2c:
-          cdt.g0 = data[cdt.offset + 3];
-          cdt.offset += 4;
-          break;
-        default:
-          cdt.g0 = data[cdt.offset + 2];
-          cdt.offset += 3;
-          break;
-        }
-        break;
-      case 0x67:
-      case 0x62:
-      case 0x70:
-        cdt.g0 = data[cdt.offset + 1];
-        cdt.offset += 2;
-        cdt.multibyte = false;
-        break;
-      case 0x73:
+    }
+
+    /**
+     * <p>
+     * Converts MARC-8 data to UCS/Unicode.
+     * </p>
+     * 
+     * @param dataElement
+     *            the MARC-8 data
+     * @return String - the UCS/Unicode data
+     */
+    public String convert(String dataElement) {
+        char[] data = null;
+        data = dataElement.toCharArray();
+        StringBuffer sb = new StringBuffer();
+        int len = data.length;
+
+        CodeTracker cdt = new CodeTracker();
+
         cdt.g0 = 0x42;
-        cdt.offset += 2;
+        cdt.g1 = 0x45;
         cdt.multibyte = false;
-        break;
-      }
-    }
-  }
 
-  /**
-   * <p>
-   * Converts MARC-8 data to UCS/Unicode.
-   * </p>
-   * 
-   * @param dataElement
-   *          the MARC-8 data
-   * @return String - the UCS/Unicode data
-   */
-  public String convert(String dataElement) {
-    char[] data = null;
-    data = dataElement.toCharArray();
-    StringBuffer sb = new StringBuffer();
-    int len = data.length;
+        cdt.offset = 0;
 
-    CodeTracker cdt = new CodeTracker();
-
-    cdt.g0 = 0x42;
-    cdt.g1 = 0x45;
-    cdt.multibyte = false;
-
-    cdt.offset = 0;
-
-    checkMode(data, cdt);
-
-    Queue diacritics = new Queue();
-
-    while (cdt.offset < data.length) {
-      if (ct.isCombining(data[cdt.offset], cdt.g0, cdt.g1)
-          && hasNext(cdt.offset, len)) {
-
-        while (ct.isCombining(data[cdt.offset], cdt.g0, cdt.g1)
-            && hasNext(cdt.offset, len)) {
-          diacritics.put(new Character(
-              getChar(data[cdt.offset], cdt.g0, cdt.g1)));
-          cdt.offset++;
-          checkMode(data, cdt);
-        }
-
-        char c2 = getChar(data[cdt.offset], cdt.g0, cdt.g1);
-        cdt.offset++;
         checkMode(data, cdt);
-        sb.append(c2);
 
-        while (!diacritics.isEmpty()) {
-          char c1 = ((Character) diacritics.get()).charValue();
-          sb.append(c1);
+        Queue diacritics = new Queue();
+
+        while (cdt.offset < data.length) {
+            if (ct.isCombining(data[cdt.offset], cdt.g0, cdt.g1)
+                    && hasNext(cdt.offset, len)) {
+
+                while (ct.isCombining(data[cdt.offset], cdt.g0, cdt.g1)
+                        && hasNext(cdt.offset, len)) {
+                    diacritics.put(new Character(getChar(data[cdt.offset],
+                            cdt.g0, cdt.g1)));
+                    cdt.offset++;
+                    checkMode(data, cdt);
+                }
+
+                char c2 = getChar(data[cdt.offset], cdt.g0, cdt.g1);
+                cdt.offset++;
+                checkMode(data, cdt);
+                sb.append(c2);
+
+                while (!diacritics.isEmpty()) {
+                    char c1 = ((Character) diacritics.get()).charValue();
+                    sb.append(c1);
+                }
+
+            } else if (cdt.multibyte) {
+                sb.append(ct.getChar(makeMultibyte(new String(data).substring(
+                        cdt.offset, cdt.offset + 4).toCharArray()), cdt.g0));
+                cdt.offset += 3;
+            } else {
+                sb.append(getChar(data[cdt.offset], cdt.g0, cdt.g1));
+                cdt.offset += 1;
+            }
+            if (hasNext(cdt.offset, len))
+                checkMode(data, cdt);
         }
-
-      } else if (cdt.multibyte) {
-        sb.append(ct.getChar(makeMultibyte(new String(data).substring(
-            cdt.offset, cdt.offset + 4).toCharArray()), cdt.g0));
-        cdt.offset += 3;
-      } else {
-        sb.append(getChar(data[cdt.offset], cdt.g0, cdt.g1));
-        cdt.offset += 1;
-      }
-      if (hasNext(cdt.offset, len))
-        checkMode(data, cdt);
+        return sb.toString();
     }
-    return sb.toString();
-  }
 
-  private int makeMultibyte(char[] data) {
-    int[] chars = new int[3];
-    chars[0] = data[0] << 16;
-    chars[1] = data[1] << 8;
-    chars[2] = data[2];
-    return chars[0] | chars[1] | chars[2];
-  }
+    private int makeMultibyte(char[] data) {
+        int[] chars = new int[3];
+        chars[0] = data[0] << 16;
+        chars[1] = data[1] << 8;
+        chars[2] = data[2];
+        return chars[0] | chars[1] | chars[2];
+    }
 
-  private char getChar(int ch, int g0, int g1) {
-    if (ch <= 0x7E)
-      return ct.getChar(ch, g0);
-    else
-      return ct.getChar(ch, g1);
-  }
+    private char getChar(int ch, int g0, int g1) {
+        if (ch <= 0x7E)
+            return ct.getChar(ch, g0);
+        else
+            return ct.getChar(ch, g1);
+    }
 
-  private char getMBChar(int ch) {
-    return ct.getChar(ch, 0x31);
-  }
+    private char getMBChar(int ch) {
+        return ct.getChar(ch, 0x31);
+    }
 
-  private static boolean hasNext(int pos, int len) {
-    if (pos < (len - 1))
-      return true;
-    return false;
-  }
+    private static boolean hasNext(int pos, int len) {
+        if (pos < (len - 1))
+            return true;
+        return false;
+    }
 
-  private static boolean isEscape(int i) {
-    if (i == 0x1B)
-      return true;
-    return false;
-  }
+    private static boolean isEscape(int i) {
+        if (i == 0x1B)
+            return true;
+        return false;
+    }
 
 }
--- src/org/marc4j/converter/impl/UnicodeToAnsel.java
+++ src/org/marc4j/converter/impl/UnicodeToAnsel.java
@@ -1,4 +1,4 @@
-// $Id: UnicodeToAnsel.java,v 1.1 2005/05/04 10:06:46 bpeters Exp $
+// $Id: UnicodeToAnsel.java,v 1.2 2005/11/28 16:50:22 bpeters Exp $
 /**
  * Copyright (C) 2002 Bas Peters (mail@bpeters.com)
  *
@@ -20,6 +20,7 @@
  */
 package org.marc4j.converter.impl;
 
+import java.io.InputStream;
 import java.util.Hashtable;
 
 import org.marc4j.converter.CharConverter;
@@ -29,103 +30,134 @@ import org.marc4j.converter.CharConverter;
  * A utility to convert UCS/Unicode data to MARC-8.
  * </p>
  * <p>
- * The MARC-8 to Unicode mapping used is the version with the March 2005 revisions.
+ * The MARC-8 to Unicode mapping used is the version with the March 2005
+ * revisions.
  * </p>
+ * 
  * @author Bas Peters
  * @author Corey Keith
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class UnicodeToAnsel implements CharConverter {
-  protected ReverseCodeTable rct;
+    protected ReverseCodeTable rct;
 
-  static final char ESC = 0x1b;
+    static final char ESC = 0x1b;
 
-  static final char G0 = 0x28;
+    static final char G0 = 0x28;
 
-  static final char G0multibyte = 0x24;
+    static final char G0multibyte = 0x24;
 
-  static final char G1 = 0x29;
+    static final char G1 = 0x29;
 
-  static final int ASCII = 0x42;
+    static final int ASCII = 0x42;
 
-  /**
-   * Default contructor.
-   *
-   */
-  public UnicodeToAnsel() {
-    rct = new ReverseCodeTable(getClass().getResourceAsStream(
-        "resources/codetables.xml"));
-  }
+    /**
+     * Creates a new instance and loads the MARC4J supplied Ansel/Unicode
+     * conversion tables based on the official LC tables.
+     */
+    public UnicodeToAnsel() {
+        this(UnicodeToAnsel.class
+                .getResourceAsStream("resources/codetables.xml"));
+    }
 
-  /**
-   * Converts UCS/Unicode data to MARC-8.
-   * 
-   * <p>
-   * A question mark (0x3F) is returned if there is no match.
-   * </p>
-   * 
-   * @param dataElement
-   *          the UCS/Unicode data
-   * @return String - the MARC-8 data
-   */
-  public String convert(String dataElement) {
-    char[] data = dataElement.toCharArray();
-    StringBuffer sb = new StringBuffer();
-    CodeTableTracker ctt = new CodeTableTracker();
+    /**
+     * Constructs an instance with the specified pathname.
+     * 
+     * Use this constructor to create an instance with a customized code table
+     * mapping. The mapping file should follow the structure of LC's XML MARC-8
+     * to Unicode mapping (see:
+     * http://www.loc.gov/marc/specifications/codetables.xml).
+     *  
+     */
+    public UnicodeToAnsel(String pathname) {
+        rct = new ReverseCodeTable(pathname);
+    }
+    
+    /**
+     * Constructs an instance with the specified input stream.
+     * 
+     * Use this constructor to create an instance with a customized code table
+     * mapping. The mapping file should follow the structure of LC's XML MARC-8
+     * to Unicode mapping (see:
+     * http://www.loc.gov/marc/specifications/codetables.xml).
+     *  
+     */
+    public UnicodeToAnsel(InputStream in) {
+        rct = new ReverseCodeTable(in);
+    }
 
-    boolean technique1 = false;
+    /**
+     * Converts UCS/Unicode data to MARC-8.
+     * 
+     * <p>
+     * A question mark (0x3F) is returned if there is no match.
+     * </p>
+     * 
+     * @param dataElement
+     *            the UCS/Unicode data
+     * @return String - the MARC-8 data
+     */
+    public String convert(String dataElement) {
+        char[] data = dataElement.toCharArray();
+        StringBuffer sb = new StringBuffer();
+        CodeTableTracker ctt = new CodeTableTracker();
 
-    for (int i = 0; i < data.length; i++) {
-      Character c = new Character(data[i]);
-      Integer table;
-      StringBuffer marc = new StringBuffer();
-      Hashtable h = rct.codeTableHash(c);
+        boolean technique1 = false;
 
-      if (h.keySet().contains(ctt.getPrevious(CodeTableTracker.G0))) {
-        ctt.makePreviousCurrent();
-        marc.append((char[]) h.get(ctt.getPrevious(CodeTableTracker.G0)));
-      } else if (h.keySet().contains(ctt.getPrevious(CodeTableTracker.G1))) {
-        ctt.makePreviousCurrent();
-        marc.append((char[]) h.get(ctt.getPrevious(CodeTableTracker.G1)));
-      } else {
-        table = (Integer) h.keySet().iterator().next();
-        char[] marc8 = (char[]) h.get(table);
+        for (int i = 0; i < data.length; i++) {
+            Character c = new Character(data[i]);
+            Integer table;
+            StringBuffer marc = new StringBuffer();
+            Hashtable h = rct.codeTableHash(c);
 
-        if (marc8.length == 3) {
-          marc.append(ESC);
-          marc.append(G0multibyte);
-          ctt.setPrevious(CodeTableTracker.G0, table);
-        } else if (marc8[0] < 0x80) {
-          marc.append(ESC);
-          if ((table.intValue() == 0x62) || (table.intValue() == 0x70)) {
-            technique1 = true;
-          } else {
-            marc.append(G0);
-          }
-          ctt.setPrevious(CodeTableTracker.G0, table);
-        } else {
-          marc.append(ESC);
-          marc.append(G1);
-          ctt.setPrevious(CodeTableTracker.G1, table);
+            if (h.keySet().contains(ctt.getPrevious(CodeTableTracker.G0))) {
+                ctt.makePreviousCurrent();
+                marc.append((char[]) h
+                        .get(ctt.getPrevious(CodeTableTracker.G0)));
+            } else if (h.keySet()
+                    .contains(ctt.getPrevious(CodeTableTracker.G1))) {
+                ctt.makePreviousCurrent();
+                marc.append((char[]) h
+                        .get(ctt.getPrevious(CodeTableTracker.G1)));
+            } else {
+                table = (Integer) h.keySet().iterator().next();
+                char[] marc8 = (char[]) h.get(table);
+
+                if (marc8.length == 3) {
+                    marc.append(ESC);
+                    marc.append(G0multibyte);
+                    ctt.setPrevious(CodeTableTracker.G0, table);
+                } else if (marc8[0] < 0x80) {
+                    marc.append(ESC);
+                    if ((table.intValue() == 0x62)
+                            || (table.intValue() == 0x70)) {
+                        technique1 = true;
+                    } else {
+                        marc.append(G0);
+                    }
+                    ctt.setPrevious(CodeTableTracker.G0, table);
+                } else {
+                    marc.append(ESC);
+                    marc.append(G1);
+                    ctt.setPrevious(CodeTableTracker.G1, table);
+                }
+                marc.append((char) table.intValue());
+                marc.append(marc8);
+            }
+
+            if (rct.isCombining(c))
+                sb.insert(sb.length() - 1, marc);
+            else
+                sb.append(marc);
         }
-        marc.append((char) table.intValue());
-        marc.append(marc8);
-      }
 
-      if (rct.isCombining(c))
-        sb.insert(sb.length() - 1, marc);
-      else
-        sb.append(marc);
+        if (ctt.getPrevious(CodeTableTracker.G0).intValue() != ASCII) {
+            sb.append(ESC);
+            sb.append(G0);
+            sb.append((char) ASCII);
+        }
+
+        return sb.toString();
     }
-
-    if (ctt.getPrevious(CodeTableTracker.G0).intValue() != ASCII) {
-      sb.append(ESC);
-      sb.append(G0);
-      sb.append((char) ASCII);
-    }
-
-    return sb.toString();
-  }
 
 }
-
