d012a4d22da022d0e2ffef1f49404d8568fab6c2
bpeters <bpeters>
2006-07-28 12:26:23 +0000

    Resolved issue raised by Mihály Szabó to fix encoding problem when writing records with non-latin-1 encoding.

--- src/org/marc4j/MarcStreamWriter.java
+++ src/org/marc4j/MarcStreamWriter.java
@@ -1,4 +1,4 @@
-// $Id: MarcStreamWriter.java,v 1.2 2005/08/05 17:29:55 bpeters Exp $
+// $Id: MarcStreamWriter.java,v 1.3 2006/07/28 12:26:23 bpeters Exp $
 /**
  * Copyright (C) 2004 Bas Peters
  *
@@ -20,11 +20,9 @@
  */
 package org.marc4j;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
-import java.io.Writer;
 import java.text.DecimalFormat;
 import java.util.Iterator;
 import java.util.List;
@@ -49,15 +47,15 @@ import org.marc4j.marc.Subfield;
  * MarcXmlReader reader = new MarcXmlReader(input);
  * MarcWriter writer = new MarcStreamWriter(System.out);
  * while (reader.hasNext()) {
- * 	Record record = reader.next();
- * 	writer.write(record);
+ *     Record record = reader.next();
+ *     writer.write(record);
  * }
  * writer.close();
  * </pre>
  * 
  * <p>
- * To convert characters like to converting from UCS/Unicode to MARC-8 register
- * a {@link org.marc4j.converter.CharConverter}implementation:
+ * To convert characters like for example from UCS/Unicode to MARC-8 register
+ * a {@link org.marc4j.converter.CharConverter}&nbsp;implementation:
  * </p>
  * 
  * <pre>
@@ -66,171 +64,166 @@ import org.marc4j.marc.Subfield;
  * MarcWriter writer = new MarcStreamWriter(System.out);
  * writer.setConverter(new UnicodeToAnsel());
  * while (reader.hasNext()) {
- * 	Record record = reader.next();
- * 	writer.write(record);
+ *     Record record = reader.next();
+ *     writer.write(record);
  * }
  * writer.close();
  * </pre>
  * 
  * @author Bas Peters
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 public class MarcStreamWriter implements MarcWriter {
 
-	private Writer writer = null;
+    private OutputStream out = null;
 
-	private String encoding = null;
+    private String encoding = "ISO_8859_1";
 
-	private CharConverter converter = null;
+    private CharConverter converter = null;
 
-	private static DecimalFormat format4 = new DecimalFormat("0000");
+    private static DecimalFormat format4 = new DecimalFormat("0000");
 
-	private static DecimalFormat format5 = new DecimalFormat("00000");
+    private static DecimalFormat format5 = new DecimalFormat("00000");
 
-	/**
-	 * Constructs an instance and creates a <code>Writer</code> object with
-	 * the specified output stream.
-	 */
-	public MarcStreamWriter(OutputStream out) {
-		writer = new OutputStreamWriter(out);
-	}
+    /**
+     * Constructs an instance and creates a <code>Writer</code> object with
+     * the specified output stream.
+     */
+    public MarcStreamWriter(OutputStream out) {
+        this.out = out;
+    }
 
-	/**
-	 * Constructs an instance and creates a <code>Writer</code> object with
-	 * the specified output stream and character encoding.
-	 */
-	public MarcStreamWriter(OutputStream out, String encoding) {
-		this.encoding = encoding;
-		try {
-			writer = new OutputStreamWriter(out, encoding);
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
-	}
+    /**
+     * Constructs an instance and creates a <code>Writer</code> object with
+     * the specified output stream and character encoding.
+     */
+    public MarcStreamWriter(OutputStream out, String encoding) {
+        this.encoding = encoding;
+        this.out = out;
+    }
 
-	/**
-	 * Returns the character converter.
-	 * 
-	 * @return CharConverter the character converter
-	 */
-	public CharConverter getConverter() {
-		return converter;
-	}
+    /**
+     * Returns the character converter.
+     * 
+     * @return CharConverter the character converter
+     */
+    public CharConverter getConverter() {
+        return converter;
+    }
 
-	/**
-	 * Sets the character converter.
-	 * 
-	 * @param converter
-	 *            the character converter
-	 */
-	public void setConverter(CharConverter converter) {
-		this.converter = converter;
-	}
+    /**
+     * Sets the character converter.
+     * 
+     * @param converter
+     *            the character converter
+     */
+    public void setConverter(CharConverter converter) {
+        this.converter = converter;
+    }
 
-	/**
-	 * Returns the <code>Writer</code> instance.
-	 * 
-	 * @return Writer - the writer instance
-	 */
-	public Writer getWriter() {
-		return writer;
-	}
+    /**
+     * Writes a <code>Record</code> object to the writer.
+     * 
+     * @param record -
+     *            the <code>Record</code> object
+     */
+    public void write(Record record) {
+        int previous = 0;
 
-	/**
-	 * Writes a <code>Record</code> object to the writer.
-	 * 
-	 * @param record -
-	 *            the <code>Record</code> object
-	 */
-	public void write(Record record) {
-		int previous = 0;
-		StringBuffer data = new StringBuffer();
-		StringBuffer dir = new StringBuffer();
+        try {
+            ByteArrayOutputStream data = new ByteArrayOutputStream();
+            ByteArrayOutputStream dir = new ByteArrayOutputStream();
 
-		// control fields
-		List fields = record.getControlFields();
-		Iterator i = fields.iterator();
-		while (i.hasNext()) {
-			ControlField cf = (ControlField) i.next();
-			data.append(getDataElement(cf.getData()));
-			data.append((char) Constants.FT);
-			dir
-					.append(getEntry(cf.getTag(), data.length() - previous,
-							previous));
-			previous = data.length();
-		}
+            // control fields
+            List fields = record.getControlFields();
+            Iterator i = fields.iterator();
+            while (i.hasNext()) {
+                ControlField cf = (ControlField) i.next();
 
-		// data fields
-		fields = record.getDataFields();
-		i = fields.iterator();
-		while (i.hasNext()) {
-			DataField df = (DataField) i.next();
-			data.append(df.getIndicator1());
-			data.append(df.getIndicator2());
-			List subfields = df.getSubfields();
-			Iterator si = subfields.iterator();
-			while (si.hasNext()) {
-				Subfield sf = (Subfield) si.next();
-				data.append((char) Constants.US);
-				data.append(sf.getCode());
-				data.append(getDataElement(sf.getData()));
-			}
-			data.append((char) Constants.FT);
-			dir
-					.append(getEntry(df.getTag(), data.length() - previous,
-							previous));
-			previous = data.length();
-		}
-		dir.append((char) Constants.FT);
+                data.write(getDataElement(cf.getData()));
+                data.write(Constants.FT);
+                dir.write(getEntry(cf.getTag(), data.size() - previous,
+                        previous));
+                previous = data.size();
+            }
 
-		// base address of data and logical record length
-		Leader ldr = record.getLeader();
-		ldr.setBaseAddressOfData(24 + dir.length());
-		ldr.setRecordLength(ldr.getBaseAddressOfData() + data.length() + 1);
+            // data fields
+            fields = record.getDataFields();
+            i = fields.iterator();
+            while (i.hasNext()) {
+                DataField df = (DataField) i.next();
+                data.write(df.getIndicator1());
+                data.write(df.getIndicator2());
+                List subfields = df.getSubfields();
+                Iterator si = subfields.iterator();
+                while (si.hasNext()) {
+                    Subfield sf = (Subfield) si.next();
+                    data.write(Constants.US);
+                    data.write(sf.getCode());
+                    data.write(getDataElement(sf.getData()));
+                }
+                data.write(Constants.FT);
+                dir.write(getEntry(df.getTag(), data.size() - previous,
+                        previous));
+                previous = data.size();
+            }
+            dir.write(Constants.FT);
 
-		// write record to output stream
-		try {
-			write(ldr);
-			writer.write(dir.toString());
-			writer.write(data.toString());
-			writer.write(Constants.RT);
-		} catch (IOException e) {
-			throw new MarcException("IO Error occured while writing record", e);
-		}
-	}
+            // base address of data and logical record length
+            Leader ldr = record.getLeader();
 
-	private void write(Leader ldr) throws IOException {
-		writer.write(format5.format(ldr.getRecordLength()));
-		writer.write(ldr.getRecordStatus());
-		writer.write(ldr.getTypeOfRecord());
-		writer.write(ldr.getImplDefined1());
-		writer.write(ldr.getCharCodingScheme());
-		writer.write(Integer.toString(ldr.getIndicatorCount()));
-		writer.write(Integer.toString(ldr.getSubfieldCodeLength()));
-		writer.write(format5.format(ldr.getBaseAddressOfData()));
-		writer.write(ldr.getImplDefined2());
-		writer.write(ldr.getEntryMap());
-	}
+            ldr.setBaseAddressOfData(24 + dir.size());
+            ldr.setRecordLength(ldr.getBaseAddressOfData() + data.size() + 1);
 
-	/**
-	 * Closes the writer.
-	 */
-	public void close() {
-		try {
-			writer.close();
-		} catch (IOException e) {
-			throw new MarcException("IO Error occured on close", e);
-		}
-	}
+            // write record to output stream
+            dir.close();
+            data.close();
+            write(ldr);
+            out.write(dir.toByteArray());
+            out.write(data.toByteArray());
+            out.write(Constants.RT);
 
-	private String getDataElement(String data) {
-		if (converter != null)
-			return converter.convert(data);
-		return data;
-	}
+        } catch (IOException e) {
+            throw new MarcException("IO Error occured while writing record", e);
+        }
+    }
 
-	private String getEntry(String tag, int length, int start) {
-		return tag + format4.format(length) + format5.format(start);
-	}
+    private void write(Leader ldr) throws IOException {
+        out.write(format5.format(ldr.getRecordLength()).getBytes(encoding));
+        out.write(ldr.getRecordStatus());
+        out.write(ldr.getTypeOfRecord());
+        out.write(new String(ldr.getImplDefined1()).getBytes(encoding));
+        out.write(ldr.getCharCodingScheme());
+        out.write(Integer.toString(ldr.getIndicatorCount()).getBytes(encoding));
+        out.write(Integer.toString(ldr.getSubfieldCodeLength()).getBytes(
+                encoding));
+        out
+                .write(format5.format(ldr.getBaseAddressOfData()).getBytes(
+                        encoding));
+        out.write(new String(ldr.getImplDefined2()).getBytes(encoding));
+        out.write(new String(ldr.getEntryMap()).getBytes(encoding));
+    }
 
+    /**
+     * Closes the writer.
+     */
+    public void close() {
+        try {
+            out.close();
+        } catch (IOException e) {
+            throw new MarcException("IO Error occured on close", e);
+        }
+    }
+
+    private byte[] getDataElement(String data) throws IOException {
+        if (converter != null)
+            return converter.convert(data).getBytes(encoding);
+        return data.getBytes(encoding);
+    }
+
+    private byte[] getEntry(String tag, int length, int start)
+            throws IOException {
+        return (tag + format4.format(length) + format5.format(start))
+                .getBytes(encoding);
+    }
 }
