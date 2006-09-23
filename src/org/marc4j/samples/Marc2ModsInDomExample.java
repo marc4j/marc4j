f8c0de20e07e08e17207b35191c7e59a0ac32eff
bpeters <bpeters>
2006-09-23 10:08:58 +0000

    Initial import

---
+++ src/org/marc4j/samples/Marc2ModsInDomExample.java
@@ -0,0 +1,68 @@
+// $Id: Marc2ModsInDomExample.java,v 1.1 2006/09/23 10:09:30 bpeters Exp $
+/**
+ * Copyright (C) 2002-2006 Bas Peters
+ *
+ * This file is part of MARC4J
+ *
+ * MARC4J is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public 
+ * License as published by the Free Software Foundation; either 
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * MARC4J is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with MARC4J; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.marc4j.samples;
+
+import java.io.InputStream;
+
+import javax.xml.transform.Source;
+import javax.xml.transform.dom.DOMResult;
+import javax.xml.transform.stream.StreamSource;
+
+import org.marc4j.MarcReader;
+import org.marc4j.MarcStreamReader;
+import org.marc4j.MarcXmlWriter;
+import org.marc4j.converter.impl.AnselToUnicode;
+import org.marc4j.marc.Record;
+import org.w3c.dom.Document;
+
+/**
+ * Writes MARC XML to a DOM document.
+ * 
+ * @author Bas Peters
+ * @version $Revision: 1.1 $
+ */
+public class Marc2ModsInDomExample {
+
+    public static void main(String args[]) throws Exception {
+
+        InputStream input = ReadMarcExample.class
+                .getResourceAsStream("resources/summerland.mrc");
+        
+        MarcReader reader = new MarcStreamReader(input);
+
+        String stylesheetUrl = "http://www.loc.gov/standards/mods/v3/MARC21slim2MODS3.xsl";
+        Source stylesheet = new StreamSource(stylesheetUrl);
+        
+        DOMResult result = new DOMResult();
+        MarcXmlWriter writer = new MarcXmlWriter(result, stylesheet);
+        writer.setConverter(new AnselToUnicode());
+        while (reader.hasNext()) {
+            Record record = (Record) reader.next();
+            writer.write(record);
+        }
+        writer.close();
+
+        Document doc = (Document) result.getNode();
+        
+        System.out.println(doc.getDocumentElement().getLocalName());
+
+    }
+}
---
+++ src/org/marc4j/samples/RemoveLocalFieldsExample.java
@@ -0,0 +1,68 @@
+// $Id: RemoveLocalFieldsExample.java,v 1.1 2006/09/23 10:10:08 bpeters Exp $
+/**
+ * Copyright (C) 2002-2006 Bas Peters
+ *
+ * This file is part of MARC4J
+ *
+ * MARC4J is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public 
+ * License as published by the Free Software Foundation; either 
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * MARC4J is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with MARC4J; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.marc4j.samples;
+
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.marc4j.MarcReader;
+import org.marc4j.MarcStreamReader;
+import org.marc4j.marc.DataField;
+import org.marc4j.marc.Record;
+
+/**
+ * Removes local field (tag 9XX).
+ * 
+ * @author Bas Peters
+ * @version $Revision: 1.1 $
+ */
+public class RemoveLocalFieldsExample {
+
+    public static void main(String args[]) throws Exception {
+
+        InputStream input = RemoveLocalFieldsExample.class
+                .getResourceAsStream("resources/chabon-loc.mrc");
+
+        MarcReader reader = new MarcStreamReader(input);
+        while (reader.hasNext()) {
+            Record record = reader.next();
+            System.out.println(record.toString());
+
+            Pattern pattern = Pattern.compile("9\\d\\d");
+
+            List fields = record.getDataFields();
+
+            Iterator i = fields.iterator();
+            while (i.hasNext()) {
+                DataField field = (DataField) i.next();
+                Matcher matcher = pattern.matcher(field.getTag());
+                if (matcher.matches())
+                    i.remove();
+            }
+            System.out.println(record.toString());
+        }
+
+    }
+
+}
