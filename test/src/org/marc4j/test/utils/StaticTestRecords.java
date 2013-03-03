7a720a0af26feccfe9a3c0d389b927aaef064174
Simon Spero <sesuncedu@gmail.com>
2013-03-02 19:58:52 -0500

    Create new copies of Summerland record when getSummerlandRecord is called (to avoid interference) when tests are running in same VM

--- test/src/org/marc4j/test/utils/StaticTestRecords.java
+++ test/src/org/marc4j/test/utils/StaticTestRecords.java
@@ -4,8 +4,7 @@ import org.marc4j.marc.MarcFactory;
 import org.marc4j.marc.Record;
 
 
-public class StaticTestRecords
-{
+public class StaticTestRecords {
     public static final String RESOURCES_ILLEGAL_MARC_IN_JSON_JSON = "/illegal-marc-in-json.json";
     public static final String RESOURCES_MARC_IN_JSON_JSON = "/marc-in-json.json";
     public static final String RESOURCES_MARC_JSON_JSON = "/marc-json.json";
@@ -26,8 +25,7 @@ public class StaticTestRecords
     public static Record summerland[] = new Record[1];
     private static MarcFactory factory;
 
-    static 
-    {
+    static {
         factory = MarcFactory.newInstance();
         chabon[0] = factory.newRecord("00759cam a2200229 a 4500");
         chabon[0].addVariableField(factory.newControlField("001", "11939876"));
@@ -64,29 +62,33 @@ public class StaticTestRecords
         chabon[1].addVariableField(factory.newDataField("650", ' ', '1', "a", "Fantasy."));
         chabon[1].addVariableField(factory.newDataField("650", ' ', '1', "a", "Baseball", "v", "Fiction."));
         chabon[1].addVariableField(factory.newDataField("650", ' ', '1', "a", "Magic", "v", "Fiction."));
-        
-        summerland[0] = factory.newRecord("00714cam a2200205 a 4500");
-        summerland[0].addVariableField(factory.newControlField("001", "12883376"));
-        summerland[0].addVariableField(factory.newControlField("005", "20030616111422.0"));
-        summerland[0].addVariableField(factory.newControlField("008", "020805s2002    nyu    j      000 1 eng  "));
-        summerland[0].addVariableField(factory.newDataField("020", ' ', ' ', "a", "0786808772"));
-        summerland[0].addVariableField(factory.newDataField("020", ' ', ' ', "a", "0786816155 (pbk.)"));
-        summerland[0].addVariableField(factory.newDataField("040", ' ', ' ', "a", "DLC", "c", "DLC", "d", "DLC"));
-        summerland[0].addVariableField(factory.newDataField("100", '1', ' ', "a", "Chabon, Michael."));
-        summerland[0].addVariableField(factory.newDataField("245", '1', '0', "a", "Summerland /", "c", "Michael Chabon."));
-        summerland[0].addVariableField(factory.newDataField("250", ' ', ' ', "a", "1st ed."));
-        summerland[0].addVariableField(factory.newDataField("260", ' ', ' ', "a", "New York :", "b", "Miramax Books/Hyperion Books for Children,", "c", "c2002."));
-        summerland[0].addVariableField(factory.newDataField("300", ' ', ' ', "a", "500 p. ;", "c", "22 cm."));
-        summerland[0].addVariableField(factory.newDataField("520", ' ', ' ', "a", "Ethan Feld, the worst baseball player in the history of the game, finds himself recruited by a 100-year-old scout to help a band of fairies triumph over an ancient enemy."));
-        summerland[0].addVariableField(factory.newDataField("650", ' ', '1', "a", "Fantasy."));
-        summerland[0].addVariableField(factory.newDataField("650", ' ', '1', "a", "Baseball", "v", "Fiction."));
-        summerland[0].addVariableField(factory.newDataField("650", ' ', '1', "a", "Magic", "v", "Fiction."));
 
+        summerland[0] = makeSummerlandRecord();
+    }
+
+    private static Record makeSummerlandRecord() {
+        Record sumland = factory.newRecord("00714cam a2200205 a 4500");
+        sumland.addVariableField(factory.newControlField("001", "12883376"));
+        sumland.addVariableField(factory.newControlField("005", "20030616111422.0"));
+        sumland.addVariableField(factory.newControlField("008", "020805s2002    nyu    j      000 1 eng  "));
+        sumland.addVariableField(factory.newDataField("020", ' ', ' ', "a", "0786808772"));
+        sumland.addVariableField(factory.newDataField("020", ' ', ' ', "a", "0786816155 (pbk.)"));
+        sumland.addVariableField(factory.newDataField("040", ' ', ' ', "a", "DLC", "c", "DLC", "d", "DLC"));
+        sumland.addVariableField(factory.newDataField("100", '1', ' ', "a", "Chabon, Michael."));
+        sumland.addVariableField(factory.newDataField("245", '1', '0', "a", "Summerland /", "c", "Michael Chabon."));
+        sumland.addVariableField(factory.newDataField("250", ' ', ' ', "a", "1st ed."));
+        sumland.addVariableField(factory.newDataField("260", ' ', ' ', "a", "New York :", "b", "Miramax Books/Hyperion Books for Children,", "c", "c2002."));
+        sumland.addVariableField(factory.newDataField("300", ' ', ' ', "a", "500 p. ;", "c", "22 cm."));
+        sumland.addVariableField(factory.newDataField("520", ' ', ' ', "a", "Ethan Feld, the worst baseball player in the history of the game, finds himself recruited by a 100-year-old scout to help a band of fairies triumph over an ancient enemy."));
+        sumland.addVariableField(factory.newDataField("650", ' ', '1', "a", "Fantasy."));
+        sumland.addVariableField(factory.newDataField("650", ' ', '1', "a", "Baseball", "v", "Fiction."));
+        sumland.addVariableField(factory.newDataField("650", ' ', '1', "a", "Magic", "v", "Fiction."));
+        return sumland;
     }
 
 
     public static Record getSummerlandRecord() {
-        return summerland[0];
+        return makeSummerlandRecord();
     }
 
     public static MarcFactory getFactory() {
