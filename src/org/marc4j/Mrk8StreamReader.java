c46f82a075d882db8bd4d9ab45e76d46eb4cf65c
rh9ec <rh9ec@virginia.edu>
2016-12-10 22:41:37 -0500

    Copying over new Mrk8 Stream Reader and Writer from freelib branch

---
+++ src/org/marc4j/Mrk8StreamReader.java
@@ -0,0 +1,181 @@
+/**
+ * Copyright (C) 2016 Binaek Sarkar
+ *
+ * This file is part of MARC4J
+ *
+ * MARC4J is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * MARC4J is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with MARC4J; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package org.marc4j;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+import org.marc4j.marc.DataField;
+import org.marc4j.marc.Leader;
+import org.marc4j.marc.MarcFactory;
+import org.marc4j.marc.Record;
+import org.marc4j.marc.Subfield;
+import org.marc4j.marc.VariableField;
+
+/**
+ * An iterator over a collection of MARC records in MRK8 format.
+ * <p>
+ * Example usage:
+ * <pre>
+ * InputStream input = new FileInputStream(&quot;file.mrk8&quot;);
+ * MarcReader reader = new MarcStreamReader(input);
+ * while (reader.hasNext()) {
+ *     Record record = reader.next();
+ *     // Process record
+ * }
+ * </pre>
+ * <p>
+ * Check the {@link org.marc4j.marc}&nbsp;package for examples about the use of
+ * the {@link org.marc4j.marc.Record} &nbsp;object model.
+ * </p>
+ *
+ * @author Binaek Sarkar
+ */
+public class Mrk8StreamReader implements MarcReader {
+
+    private final Scanner input;
+
+    private final MarcFactory factory;
+
+    private String lastLineRead;
+
+    /**
+     * Constructs an instance with the specified input stream.
+     *
+     * @param input
+     */
+    public Mrk8StreamReader(final InputStream input) {
+        this.input = new Scanner(new BufferedInputStream(input), StandardCharsets.UTF_8.name());
+        this.factory = MarcFactory.newInstance();
+    }
+
+    /**
+     * Returns true if the iteration has more records, false otherwise.
+     *
+     * @return Returns true if the iteration has more records, false otherwise.
+     */
+    @Override
+    public boolean hasNext() {
+        return this.input.hasNextLine();
+    }
+
+    /**
+     * Returns the next record in the given {@link InputStream}
+     *
+     * @return Record - the record object
+     */
+    @Override
+    public Record next() {
+        final List<String> lines = new ArrayList<String>();
+        if (!this.hasNext()) {
+            return null;
+        }
+        if (this.lastLineRead != null && this.lastLineRead.substring(1, 4).equalsIgnoreCase("LDR")) {
+            lines.add(lastLineRead);
+        }
+        while (this.input.hasNextLine()) {
+            final String line = this.input.nextLine();
+
+            if (line.trim().length() == 0) {
+                // this is a blank line. We do not need it
+                continue;
+            }
+            this.lastLineRead = line;
+            if (line.substring(1, 4).equalsIgnoreCase("LDR") && lines.size() > 0) {
+                // we have reached the next record... break for parsing;
+                break;
+            }
+
+            lines.add(line);
+        }
+        return this.parse(lines);
+    }
+
+    protected Record parse(final List<String> lines) {
+        if (lines == null || lines.isEmpty()) {
+            return null;
+        }
+
+        final Record record = this.factory.newRecord();
+
+        for (final String line : lines) {
+            if (line.trim().length() == 0) {
+                continue;
+            }
+
+            final String tag = line.substring(1, 4);
+
+            if (tag.equalsIgnoreCase("LDR")) {
+                record.setLeader(getLeader(line.substring(6)));
+            } else {
+                final VariableField field;
+                if (this.isControlField(tag)) {
+                    field = this.factory.newControlField(tag, line.substring(6));
+                } else {
+                    // this is obviously a data field
+                    final String data = line.substring(6);
+
+                    final char indicator1 = (data.startsWith("\\", 0) ? ' ' : data.charAt(0));
+                    final char indicator2 = (data.startsWith("\\", 1) ? ' ' : data.charAt(1));
+
+                    if (!this.isValidIndicator(indicator1) || !this.isValidIndicator(indicator2)) {
+                        throw new MarcException(
+                                "Wrong indicator format. It has to be a number or a space");
+                    }
+
+                    field = this.factory.newDataField(tag, indicator1, indicator2);
+
+                    final List<String> subs = Arrays.asList(data.substring(3).split("\\$"));
+
+                    for (String sub : subs) {
+                        final Subfield subfield = this.factory.newSubfield(sub.charAt(0), sub
+                                .substring(1));
+                        ((DataField) field).addSubfield(subfield);
+                    }
+                }
+                record.addVariableField(field);
+            }
+        }
+
+        return record;
+    }
+
+    protected boolean isValidIndicator(final char indicator) {
+        return (indicator == ' ' || (indicator >= '0' && indicator <= '9'));
+    }
+
+    protected Leader getLeader(final String substring) {
+        final Leader leader = this.factory.newLeader();
+        leader.unmarshal(substring);
+        return null;
+    }
+
+    protected boolean isControlField(final String tag) {
+        // can probably be replaced with (Integer.parseInt(tag)<10)
+        return ((tag.length() == 3) && tag.startsWith("00") && (tag.charAt(2) >= '0') && (tag
+                .charAt(2) <= '9'));
+    }
+}
---
+++ src/org/marc4j/Mrk8StreamWriter.java
@@ -0,0 +1,153 @@
+/**
+ * Copyright (C) 2016 Binaek Sarkar
+ *
+ * This file is part of MARC4J
+ *
+ * MARC4J is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * MARC4J is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with MARC4J; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.marc4j;
+
+import java.io.Closeable;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.nio.CharBuffer;
+import java.nio.charset.CharacterCodingException;
+import java.nio.charset.CharsetEncoder;
+import java.nio.charset.StandardCharsets;
+import org.marc4j.converter.CharConverter;
+import org.marc4j.marc.ControlField;
+import org.marc4j.marc.DataField;
+import org.marc4j.marc.Leader;
+import org.marc4j.marc.Record;
+import org.marc4j.marc.Subfield;
+import org.marc4j.marc.VariableField;
+
+/**
+ * Class for writing MARC record objects in MRK8 format.
+ * <p>
+ * The following example reads a file with MARCXML records and outputs the
+ * record set in MRK8 format:
+ * </p>
+ *
+ * <pre>
+ * InputStream input = new FileInputStream(&quot;marcxml.xml&quot;);
+ * MarcXmlReader reader = new MarcXmlReader(input);
+ * MarcWriter writer = new Mrk8StreamWriter(System.out);
+ * while (reader.hasNext()) {
+ *     Record record = reader.next();
+ *     writer.write(record);
+ * }
+ * writer.close();
+ * </pre>
+ *
+ *
+ * @author Binaek Sarkar
+ */
+public class Mrk8StreamWriter implements MarcWriter, Closeable {
+
+    private final PrintWriter mrk8Writer;
+    private final CharsetEncoder encoder;
+
+    /**
+     * Constructs an instance and creates a {@link MarcWriter} object with the
+     * specified output stream.
+     * 
+     * @param output The {@link OutputStream} to write to
+     */
+    public Mrk8StreamWriter(final OutputStream output) {
+        this.encoder = StandardCharsets.UTF_8.newEncoder();
+        this.mrk8Writer = new PrintWriter(output);
+    }
+
+    /**
+     * Writes a {@link Record} object to the given {@link OutputStream}
+     *
+     * @param record - the <code>Record</code> object
+     */
+    @Override
+    public void write(final Record record) {
+        final StringBuilder recordStringBuilder = new StringBuilder();
+
+        final Leader ldr = record.getLeader();
+        recordStringBuilder.append("=").append("LDR").append("  ").append(ldr.marshal());
+
+        for (final VariableField field : record.getVariableFields()) {
+            recordStringBuilder.append("=").append(field.getTag()).append("  ");
+
+            if (field instanceof ControlField) {
+                final ControlField controlField = (ControlField) field;
+                String data;
+                try {
+                    data = this.encoder.encode(CharBuffer.wrap(controlField.getData())).asCharBuffer().toString();
+                } catch (CharacterCodingException cce) {
+                    data = controlField.getData();
+                }
+                recordStringBuilder.append(data);
+            } else if (field instanceof DataField) {
+                final DataField dataField = (DataField) field;
+                recordStringBuilder.append((dataField.getIndicator1() == ' ') ? "\\" : dataField.getIndicator1());
+                recordStringBuilder.append((dataField.getIndicator2() == ' ') ? "\\" : dataField.getIndicator2());
+
+                for (final Subfield subField : dataField.getSubfields()) {
+                    String data;
+                    try {
+                        data = this.encoder.encode(CharBuffer.wrap(subField.getData())).asCharBuffer().toString();
+                    } catch (CharacterCodingException cce) {
+                        data = subField.getData();
+                    }
+                    recordStringBuilder.append("$").append(subField.getCode()).append(data);
+                }
+            }
+            recordStringBuilder.append(System.lineSeparator());
+        }
+        recordStringBuilder.append(System.lineSeparator());
+
+        this.mrk8Writer.append(recordStringBuilder);
+        this.mrk8Writer.flush();
+    }
+
+    /**
+     * This should set the character converter.
+     *
+     * However, since the {@link Mrk8StreamWriter} uses the UTF-8 encoder from
+     * {@link StandardCharsets} this method has no effect
+     *
+     * @param converter the character converter
+     */
+    @Override
+    public void setConverter(final CharConverter converter) {
+    }
+
+    /**
+     * Returns the character converter.
+     *
+     * In this case, always returns <code>null</code>
+     *
+     * @return CharConverter always <code>null</code>
+     */
+    @Override
+    public CharConverter getConverter() {
+        return null;
+    }
+
+    /**
+     * Closes the writer and the underlying {@link OutputStream}
+     */
+    @Override
+    public void close() {
+        this.mrk8Writer.flush();
+        this.mrk8Writer.close();
+    }
+}
