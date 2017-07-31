e3c6d1b5e902603bc17c2df94621524d6b50e95d
Robert Haschart <rh9ec@virginia.edu>
2017-07-31 17:27:38 -0400

    Optionally apply a provided character converter as record is output.

--- src/org/marc4j/MarcTxtWriter.java
+++ src/org/marc4j/MarcTxtWriter.java
@@ -6,7 +6,11 @@ import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 
 import org.marc4j.converter.CharConverter;
+import org.marc4j.marc.ControlField;
+import org.marc4j.marc.DataField;
 import org.marc4j.marc.Record;
+import org.marc4j.marc.Subfield;
+import org.marc4j.marc.VariableField;
 
 public class MarcTxtWriter implements MarcWriter {
     /**
@@ -15,6 +19,8 @@ public class MarcTxtWriter implements MarcWriter {
     private String indexkeyprefix = null;
 
     private PrintWriter out = null;
+
+    private CharConverter conv = null;
 
     public MarcTxtWriter(OutputStream os) {
         this.out = new PrintWriter(new OutputStreamWriter(os, StandardCharsets.UTF_8), true);
@@ -27,12 +33,24 @@ public class MarcTxtWriter implements MarcWriter {
 
     @Override
     public void write(Record record) {
-        String recStr = record.toString();
+        String recStr;
+
+        if (conv != null) {
+            recStr = applyConverter(record, conv);
+        }
+        else {
+            recStr = record.toString();
+        }
         if (indexkeyprefix != null) {
             String lines[] = recStr.split("\r?\n");
             for (String line : lines) {
-                if (line.substring(0,3).matches(indexkeyprefix)) {
+                if (line.length() >= 3 && indexkeyprefix.contains(line.substring(0,3))) {
                     out.println(line);
+                }
+            }
+            if (indexkeyprefix.contains("err") && record.getErrors() != null) {
+                for (MarcError err : record.getErrors()) {
+                    out.println(err.toString());
                 }
             }
         }
@@ -41,13 +59,41 @@ public class MarcTxtWriter implements MarcWriter {
         }
     }
 
+    private String applyConverter(Record record, CharConverter conv) {
+        final StringBuilder sb = new StringBuilder();
+        sb.append("LEADER ");
+        sb.append(record.getLeader().toString());
+        sb.append('\n');
+    
+        for (final VariableField field : record.getVariableFields()) {
+            if (field instanceof ControlField) {
+                sb.append(field.toString());
+            }
+            else if (field instanceof DataField) {
+                DataField df = (DataField)field;
+                sb.append(df.getTag());
+                sb.append(' ');
+                sb.append(df.getIndicator1());
+                sb.append(df.getIndicator2());
+
+                for (final Subfield sf : df.getSubfields()) {
+                    sb.append("$").append(sf.getCode()).append(conv.convert(sf.getData()));
+                }
+            }
+            sb.append('\n');
+        }
+        return sb.toString();
+    }
+
+
     @Override
-    public void setConverter(CharConverter converter) {        
+    public void setConverter(CharConverter converter) { 
+        conv = converter;
     }
 
     @Override
     public CharConverter getConverter() {
-        return null;
+        return conv;
     }
 
     @Override
